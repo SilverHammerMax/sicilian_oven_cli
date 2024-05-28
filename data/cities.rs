2adbd3b9744208498bd04640d5443a41bc7ef4fb
SilverHammerMax <github@flibig.com>
2024-05-27 22:37:07 -0400

    Revert to Initial Commit

--- data/cities.rs
+++
@@ -1,6 +0,0 @@
-use crate::types::city::City;
-use lazy_static::lazy_static;
-use std::collections::HashMap;
-
-lazy_static! {
-    pub static ref CITIES: HashMap<&'static str, City> = HashMap::from([

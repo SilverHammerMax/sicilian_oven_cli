8eab7d0f915571439febaf6cb4457c37f373e8cb
SilverHammerMax <github@flibig.com>
2024-06-14 23:57:21 -0400

    Add AGC and GGC to Tires

--- src/types/car/tire.rs
+++ src/types/car/tire.rs
@@ -7,3 +7,25 @@ pub enum Tire {
     TireFour,
     TireFive,
 }
+
+impl Tire {
+    pub fn get_agc(&self) -> f64 {
+        match self {
+            Tire::TireOne => 1.0,
+            Tire::TireTwo => 4.0,
+            Tire::TireThree => 6.0,
+            Tire::TireFour => 8.0,
+            Tire::TireFive => 10.0,
+        }
+    }
+
+    pub fn get_ggc(&self) -> f64 {
+        match self {
+            Tire::TireOne => 10.0,
+            Tire::TireTwo => 8.0,
+            Tire::TireThree => 6.0,
+            Tire::TireFour => 4.0,
+            Tire::TireFive => 1.0,
+        }
+    }
+}

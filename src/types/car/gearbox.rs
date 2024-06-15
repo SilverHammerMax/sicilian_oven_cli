3e48a1d8e2352e38dc7e9bfe5b0ad94fe961c9b1
SilverHammerMax <github@flibig.com>
2024-06-15 11:08:54 -0400

    Add Brake Horsepower and Deterioration to Gearboxes

--- src/types/car/gearbox.rs
+++ src/types/car/gearbox.rs
@@ -7,3 +7,25 @@ pub enum Gearbox {
     GearboxFour,
     GearboxFive,
 }
+
+impl Gearbox {
+    pub fn brake_horsepower(&self) -> f64 {
+        match self {
+            Gearbox::GearboxOne => 0.0,
+            Gearbox::GearboxTwo => 10.0,
+            Gearbox::GearboxThree => 20.0,
+            Gearbox:: GearboxFour => 30.0,
+            Gearbox::GearboxFive => 45.0,
+        }
+    }
+
+    pub fn deterioration(&self) -> f64 {
+        match self {
+            Gearbox::GearboxOne => 0.01,
+            Gearbox::GearboxTwo => 0.015,
+            Gearbox::GearboxThree => 0.02,
+            Gearbox::GearboxFour => 0.025,
+            Gearbox::GearboxFive => 0.035,
+        }
+    }
+}

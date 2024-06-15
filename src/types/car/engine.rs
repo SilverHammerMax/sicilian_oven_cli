09e90b945f06cb26fc90cb45fbbd08daf7511d88
SilverHammerMax <github@flibig.com>
2024-06-15 10:57:25 -0400

    Add Brake Horsepower and Weight to Engines

--- src/types/car/engine.rs
+++ src/types/car/engine.rs
@@ -7,3 +7,25 @@ pub enum Engine {
     EngineFour,
     EngineFive,
 }
+
+impl Engine {
+    pub fn get_brake_horsepower(&self) -> f64 {
+        match self {
+            Engine::EngineOne => 112.0,
+            Engine::EngineTwo => 200.0,
+            Engine::EngineThree => 220.0,
+            Engine::EngineFour => 276.0,
+            Engine::EngineFive => 320.0,
+        }
+    }
+
+    pub fn get_weight(&self) -> f64 {
+        match self {
+            Engine::EngineOne => 235.0,
+            Engine::EngineTwo => 357.0,
+            Engine::EngineThree => 372.0,
+            Engine::EngineFour => 406.0,
+            Engine::EngineFive => 432.0,
+        }
+    }
+}
